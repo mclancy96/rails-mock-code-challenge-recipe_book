5ebbc30c99a866ff1b563ec3ea574ffaeee44b2d
Nihar Patel <niharpatel52@gmail.com>
2025-08-26 08:45:35 -0400

    Create Chef and Recipe index and show pages

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,16 @@
-
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show, :edit, :update, :destroy]
+
   def index
     @chefs = Chef.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_chef
+      @chef = Chef.find(params[:id])
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,16 @@
-
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  
   def index
     @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_recipe
+      @recipe = Recipe.find(params[:id])
   end
 end
